098dd0c4a19ab66d6b0ab7df549e96180f1a3c7b
Max <MaxHalleran@gmail.com>
2018-10-12 22:17:20 +0000

    Added user authentication for admin resources

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,15 @@
 class Admin::DashboardController < ApplicationController
+
+  http_basic_authenticate_with name: ENV['HTTP_BASIC_AUTH_NAME'], password: ENV['HTTP_BASIC_AUTH_PASS']
+
+  def index
+   render plain: "Everyone can see me!"
+  end
+
+  def edit
+   render plain: "I'm only accessible if you know the password"
+  end
+
   def show
   end
 end
